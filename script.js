8e713df4f1d3e58cdc272b39bc73af4ec9316b49
fedtoscano <federicotoscano611@gmail.com>
2024-05-09 18:38:11 +0200

    added ticket logic

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let userKm = document.querySelector("input#user-km");
-let userAge = document.querySelector("input#user-age");
 
-let initialPrice = userKm * 0.267113;
+const btn = document.querySelector("button");
+const finalPriceHtml = document.getElementById('final-price');
 
-if(userAge<21){
-    let discount = ((initialPrice / 100) * 24.552);
-    let finalPrice = (initialPrice - discount).toFixed(2);
-    console.log(`Vuoi percorrere ${userKm}km. Il prezzo di listino è ${initialPrice}€. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
-} else if(userAge>63){
-        let discount = ((initialPrice / 100) * 37.893);
-        let finalPrice = (initialPrice - discount).toFixed(2);
-        console.log(`Vuoi percorrere ${userKm}km. Il prezzo di listino è ${initialPrice}€. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
-        } else {
-            console.log(`Vuoi percorrere ${userKm}km. Non hai diritto ad alcuno sconto, quindi il prezzo totale è di ${initialPrice}€`);
-        }
+function calculateDiscount(){
+
+    let userKm = document.getElementById("user-km").value;
+    let userAge = document.getElementById("user-age").value;
+    
+    let initialPrice = userKm * 0.267113;
+    let discount;
+    finalPriceHtml.innerHTML= '';
+    
+    if(userAge<21){
+        discount = ((initialPrice / 100) * 24.552)
+    }else if (userAge>63){
+        discount = ((initialPrice / 100) * 37.893);
+    }else {
+        discount = 0
+    }
+    
+    let finalPrice = initialPrice - discount
+    finalPriceHtml.innerHTML = finalPrice.toFixed(2)
+}
+
+btn.addEventListener('click', calculateDiscount);
+
