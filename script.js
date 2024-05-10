f38c400c39423a3cbe80fd3a54112a3b0d7e42f5
fedtoscano <federicotoscano611@gmail.com>
2024-05-10 19:09:25 +0200

    update css

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const btn = document.querySelector("button");
 
 //* Creo la funzione per calcolare lo sconto
 function calculateDiscount(){
+
     //* Seleziono gli elementi del DOM
-    const userKm = document.getElementById("user-km").value;
-    const userAge = document.getElementById("user-age").value;
+    const userKm = Number.parseInt(document.getElementById("user-km").value, 10);
+    const userAge = Number.parseInt(document.getElementById("user-age").value, 10);
     const kmDisplay = document.getElementById("km-display")
     const ageDisplay = document.getElementById("age-display")
     const finalPriceHtml = document.getElementById('final-price');
-
+    
     //* Stabilisco il prezzo iniziale
     let initialPrice = userKm * 0.267113;
-
+    
     //* Calcolo lo sconto
     let discount;
     if(userAge<21){
@@ -26,9 +27,6 @@ function calculateDiscount(){
     let finalPrice = initialPrice - discount
     
     //* Aggiorno il DOM
-    finalPriceHtml.innerHTML= '';
-    kmDisplay.innerHTML =''
-    ageDisplay.innerHTML = ''
     kmDisplay.innerHTML = userKm + 'km'
     ageDisplay.innerHTML = userAge + ' anni'
     finalPriceHtml.innerHTML = finalPrice.toFixed(2) + '&#8364;'
